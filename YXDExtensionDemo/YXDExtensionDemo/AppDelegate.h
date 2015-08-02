8d28796ef32e16453b7eca2827510ecd4afc758d
YangXudong <yangxudongsmart@gmail.com>
2015-08-03 03:02:51 +0800

    添加Demo工程，等待完善

---
+++ YXDExtensionDemo/YXDExtensionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXDExtensionDemo
+//
+//  Created by YangXudong on 15/8/3.
+//  Copyright (c) 2015年 YangXudong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXDExtensionDemo/YXDExtensionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXDExtensionDemo
+//
+//  Created by YangXudong on 15/8/3.
+//  Copyright (c) 2015年 YangXudong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
