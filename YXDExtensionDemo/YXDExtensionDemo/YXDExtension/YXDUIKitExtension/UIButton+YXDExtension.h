43c87486c60f7fba94dbf69f6fd7aed08e318ffa
yangxudong <yangxudongsmart@gmail.com>
2015-08-05 13:25:50 +0800

    button category 添加倒计时时禁用按钮方法

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIButton+YXDExtension.h
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIButton+YXDExtension.h
@@ -5,8 +5,22 @@
 //  Copyright (c) 2015年 YangXudong. All rights reserved.
 //
 
+//默认倒计时时间
+#define YXDExtension_Verify_Button_Time                  60
+
+//可用时的标题
+#define YXDExtension_Verify_Button_Title_Available       @"获取验证码"
+
+//倒计时的标题
+#define YXDExtension_Verify_Button_Title_Unavailable     @"秒后重新获取"
+
 #import <UIKit/UIKit.h>
 
 @interface UIButton (YXDExtension)
+
+/**
+ *  接收到按钮事件后 调用此方法
+ */
+- (void) unavailable;
 
 @end
--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIButton+YXDExtension.m
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIButton+YXDExtension.m
@@ -9,4 +9,44 @@
 
 @implementation UIButton (YXDExtension)
 
+/**
+ *  接收到按钮事件后 调用此方法
+ */
+- (void) unavailable
+{
+    if (!self.userInteractionEnabled)return;
+    
+    __block int timeout = YXDExtension_Verify_Button_Time;
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
+    dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0);
+    dispatch_source_set_event_handler(_timer, ^
+                                      {
+                                          if(timeout<=0) {
+                                              //倒计时结束，关闭
+                                              dispatch_source_cancel(_timer);
+                                              dispatch_async(dispatch_get_main_queue(), ^
+                                                             {
+                                                                 [self setTitle:YXDExtension_Verify_Button_Title_Available
+                                                                       forState:UIControlStateNormal];
+                                                                 self.userInteractionEnabled = YES;
+                                                                 self.enabled = YES;
+                                                             });
+                                          } else {
+                                              int seconds = timeout % 60;
+                                              NSString *strTime = [NSString stringWithFormat:@"%.2d", seconds==0?60:seconds];
+                                              dispatch_async(dispatch_get_main_queue(), ^
+                                                             {
+                                                                 [self setTitle:[NSString stringWithFormat:@"%@%@",strTime,YXDExtension_Verify_Button_Title_Unavailable]
+                                                                       forState:UIControlStateNormal];
+                                                                 self.userInteractionEnabled = NO;
+                                                                 self.enabled = NO;
+                                                             });
+                                              timeout--;
+                                          }
+                                      });
+    
+    dispatch_resume(_timer);
+}
+
 @end
