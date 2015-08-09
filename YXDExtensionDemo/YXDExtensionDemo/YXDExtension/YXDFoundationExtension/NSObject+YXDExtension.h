009e80aa479080f34c88ae9ac78c645f4808101d
YangXudong <yangxudongsmart@gmail.com>
2015-08-09 15:55:00 +0800

    NSObject添加属性Map功能

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFoundationExtension/NSObject+YXDExtension.h
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFoundationExtension/NSObject+YXDExtension.h
@@ -16,6 +16,9 @@
 //根据数据源对象创建新对象
 + (instancetype)objectWithData:(id)data;
 
+//属性名称和接口返回值名称的对应关系 @{@"属性名称":@"返回值名称"}   子类如果需要设置，则要覆盖此方法
+- (NSDictionary *)propertyMap;
+
 //获取当前对象的属性列表
 - (NSArray *)propertyList;
 
--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFoundationExtension/NSObject+YXDExtension.m
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFoundationExtension/NSObject+YXDExtension.m
@@ -12,17 +12,17 @@ static const void *YXDExtensionNSObjectUserDataKey = &YXDExtensionNSObjectUserDa
 
 @implementation NSObject (YXDExtension)
 
--(void)postNotificationName:(NSString *)notificationName userInfo:(NSDictionary *)userInfo {
+- (void)postNotificationName:(NSString *)notificationName userInfo:(NSDictionary *)userInfo {
     [[NSNotificationCenter defaultCenter] postNotificationName:notificationName object:nil userInfo:userInfo];
 }
 
-#pragma mark - 存取userData部分
+#pragma mark - 存取部分
 
--(void)setUserData:(id)userData {
+- (void)setUserData:(id)userData {
     objc_setAssociatedObject(self, YXDExtensionNSObjectUserDataKey, userData, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
--(id)userData {
+- (id)userData {
     return objc_getAssociatedObject(self, YXDExtensionNSObjectUserDataKey);
 }
 
@@ -64,6 +64,11 @@ static const void *YXDExtensionNSObjectUserDataKey = &YXDExtensionNSObjectUserDa
 }
 
 #pragma mark -
+
+- (NSDictionary *)propertyMap {
+    //子类需要覆盖此方法
+    return nil;
+}
 
 - (NSArray *)propertyList {
     unsigned int count;
@@ -140,25 +145,34 @@ static const void *YXDExtensionNSObjectUserDataKey = &YXDExtensionNSObjectUserDa
         return self;
     }
     
-    for (NSString *valueKey in propertyList) {
-        BOOL valid;
-        
-        if ([data isKindOfClass:[NSDictionary class]]) {
-            valid = ([data valueForKey:valueKey] == nil)?NO:YES;
-        } else {
-            valid = [data respondsToSelector:NSSelectorFromString(valueKey)];
+    //属性名称直接对应返回值
+    for (NSString *propertyName in propertyList) {
+        if ([self validPropertyName:propertyName data:data]) {
+            [self setValue:[data valueForKey:propertyName] forKey:propertyName];
         }
-        
-        if (valid) {
-            id value = [data valueForKey:valueKey];
-            
-            if (value && ![value isKindOfClass:[NSNull class]]) {
-                [self setValue:value forKey:valueKey];
+    }
+    
+    //属性名称对应不同的返回值
+    NSDictionary *propertyMapDictionary = [self propertyMap];
+    if (propertyMapDictionary && propertyMapDictionary.count) {
+        for (NSString *propertyName in propertyMapDictionary) {
+            if ([self validPropertyName:[propertyMapDictionary valueForKey:propertyName] data:data]) {
+                [self setValue:[data valueForKey:[propertyMapDictionary valueForKey:propertyName]] forKey:propertyName];
             }
         }
     }
     
     return self;
+}
+
+//判断data的property是否有效
+- (BOOL)validPropertyName:(NSString *)propertyName data:(id)data {
+    if ([data isKindOfClass:[NSDictionary class]]) {
+        return ([data valueForKey:propertyName] && ![data isKindOfClass:[NSNull class]]);
+    } else {
+        return (![data isKindOfClass:[NSNull class]] && [data respondsToSelector:NSSelectorFromString(propertyName)] && ![[data valueForKey:propertyName] isKindOfClass:[NSNull class]]);
+    }
+    return NO;
 }
 
 - (NSArray *)methodList {
