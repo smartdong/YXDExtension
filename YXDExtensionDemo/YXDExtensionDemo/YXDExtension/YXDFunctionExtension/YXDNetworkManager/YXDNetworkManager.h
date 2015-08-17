030672041b4a9fcdbf3553f3822920c109d1ffbd
yangxudong <yangxudongsmart@gmail.com>
2015-08-17 19:29:29 +0800

    `

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDNetworkManager/YXDNetworkManager.h
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDNetworkManager/YXDNetworkManager.h
@@ -2,12 +2,36 @@
 //  YXDNetworkManager.h
 //  YXDExtensionDemo
 //
-//  Created by zjdd on 15/8/17.
 //  Copyright (c) 2015年 YangXudong. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSInteger, HttpMethod) {
+    GET = 0,
+    POST,
+    PUT,
+    DELETE,
+    PATCH,
+};
+
 @interface YXDNetworkManager : NSObject
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDNetworkManager/YXDNetworkManager.m
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDNetworkManager/YXDNetworkManager.m
@@ -2,12 +2,66 @@
 //  YXDNetworkManager.m
 //  YXDExtensionDemo
 //
-//  Created by zjdd on 15/8/17.
 //  Copyright (c) 2015年 YangXudong. All rights reserved.
 //
 
 #import "YXDNetworkManager.h"
+#import "AFNetworking.h"
+#import "YXDHUDManager.h"
 
 @implementation YXDNetworkManager
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#pragma mark - Init
+
++(instancetype)sharedInstance {
+    static YXDNetworkManager *networkManager = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        networkManager = [YXDNetworkManager new];
+    });
+    
+    return networkManager;
+}
+
+- (void) commonInit {
+    
+}
 
 @end
