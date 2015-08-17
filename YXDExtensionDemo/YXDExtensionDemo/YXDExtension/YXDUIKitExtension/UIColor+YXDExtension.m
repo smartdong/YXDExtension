5f3f6ed65327c9ae6f4fbfaffac4fad3e41c1baa
yangxudong <yangxudongsmart@gmail.com>
2015-08-17 11:44:23 +0800

    删掉重复代码

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIColor+YXDExtension.m
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIColor+YXDExtension.m
@@ -10,10 +10,7 @@
 @implementation UIColor (YXDExtension)
 
 + (UIColor *)randomColor {
-    CGFloat red =  (CGFloat)random() / (CGFloat)RAND_MAX;
-    CGFloat blue = (CGFloat)random() / (CGFloat)RAND_MAX;
-    CGFloat green = (CGFloat)random() / (CGFloat)RAND_MAX;
-    return [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+    return [self randomColorWithAlpha:1.0];
 }
 
 + (UIColor *)randomColorWithAlpha:(CGFloat)alpha {
