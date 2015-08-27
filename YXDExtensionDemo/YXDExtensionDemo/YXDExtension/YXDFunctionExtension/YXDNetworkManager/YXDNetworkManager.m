43041210d7d38ac5e7c2c7906f1990cd4c00b5db
YangXudong <yangxudongsmart@gmail.com>
2015-08-28 01:32:31 +0800

    网络请求类 添加初始化方法

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDNetworkManager/YXDNetworkManager.m
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDNetworkManager/YXDNetworkManager.m
@@ -9,6 +9,12 @@
 #import "AFNetworking.h"
 #import "YXDHUDManager.h"
 
+@interface YXDNetworkManager ()
+
+@property (nonatomic, strong) AFHTTPRequestOperationManager  *requsetManager;
+
+@end
+
 @implementation YXDNetworkManager
 
 
@@ -54,14 +60,17 @@
     
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        networkManager = [YXDNetworkManager new];
+        networkManager = [[YXDNetworkManager new] commonInit];
     });
     
     return networkManager;
 }
 
-- (void) commonInit {
-    
+- (instancetype) commonInit {
+    self.requsetManager = [AFHTTPRequestOperationManager new];
+    self.requsetManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html",@"text/json",@"application/json",@"text/plain",@"text/javascript",nil];
+    self.requsetManager.requestSerializer.timeoutInterval = 15; //设置超时
+    return self;
 }
 
 @end
