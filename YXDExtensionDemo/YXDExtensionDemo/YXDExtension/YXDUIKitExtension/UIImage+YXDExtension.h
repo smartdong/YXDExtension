7c00540bcc2721e5cfd0c3ee630969709a972ce9
yangxudong <yangxudongsmart@gmail.com>
2015-09-16 14:14:44 +0800

    UIImage添加gif转化图片数组方法

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIImage+YXDExtension.h
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIImage+YXDExtension.h
@@ -9,6 +9,9 @@
 
 @interface UIImage (YXDExtension)
 
+//将GIF图片转化为图片数组 但是调用此方法需要引入系统库 ImageIO
++ (NSArray *)imagesByGifData:(NSData *)gifData;
+
 + (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size;
 
 - (UIImage *)scaleToSize:(CGSize)size;
--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIImage+YXDExtension.m
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIImage+YXDExtension.m
@@ -7,7 +7,40 @@
 
 #import "UIImage+YXDExtension.h"
 
+#if __has_include(<ImageIO/ImageIO.h>)
+#import <ImageIO/ImageIO.h>
+#endif
+
 @implementation UIImage (YXDExtension)
+
++(NSArray *)imagesByGifData:(NSData *)gifData {
+#if __has_include(<ImageIO/ImageIO.h>)
+    NSMutableArray *frames = [[NSMutableArray alloc] init];
+    CGImageSourceRef src = CGImageSourceCreateWithData((CFDataRef)gifData, NULL);
+    CGFloat animationTime = 0.f;
+    if (src) {
+        size_t l = CGImageSourceGetCount(src);
+        frames = [NSMutableArray arrayWithCapacity:l];
+        for (size_t i = 0; i < l; i++) {
+            CGImageRef img = CGImageSourceCreateImageAtIndex(src, i, NULL);
+            NSDictionary *properties = (NSDictionary *)CFBridgingRelease(CGImageSourceCopyPropertiesAtIndex(src, i, NULL));
+            NSDictionary *frameProperties = [properties objectForKey:(NSString *)kCGImagePropertyGIFDictionary];
+            NSNumber *delayTime = [frameProperties objectForKey:(NSString *)kCGImagePropertyGIFUnclampedDelayTime];
+            animationTime += [delayTime floatValue];
+            if (img) {
+                [frames addObject:[UIImage imageWithCGImage:img]];
+                CGImageRelease(img);
+            }
+        }
+        CFRelease(src);
+    }
+    
+    if (frames.count) {
+        return frames;
+    }
+#endif
+    return nil;
+}
 
 + (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size {
     CGRect rect = CGRectMake(0, 0, size.width, size.height);
