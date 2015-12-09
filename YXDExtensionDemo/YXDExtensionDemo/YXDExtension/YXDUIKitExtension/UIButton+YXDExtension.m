6daaf056b22ba28862370c33668bee67c9d3e054
yangxudong <yangxudongsmart@gmail.com>
2015-12-09 13:37:40 +0800

    update

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIButton+YXDExtension.m
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDUIKitExtension/UIButton+YXDExtension.m
@@ -19,10 +19,10 @@
     __block int timeout = YXDExtension_Verify_Button_Time;
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
-    dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0);
+    dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0 * NSEC_PER_SEC, 0);
     dispatch_source_set_event_handler(_timer, ^
                                       {
-                                          if(timeout<=0) {
+                                          if(timeout <= 0) {
                                               //倒计时结束，关闭
                                               dispatch_source_cancel(_timer);
                                               dispatch_async(dispatch_get_main_queue(), ^
