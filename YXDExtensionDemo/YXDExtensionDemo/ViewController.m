001e0fd82b17e5ac2456dfb9abdebda74059bd0a
yangxudong <yangxudongsmart@gmail.com>
2015-12-17 19:43:49 +0800

    update

--- YXDExtensionDemo/YXDExtensionDemo/ViewController.m
+++ YXDExtensionDemo/YXDExtensionDemo/ViewController.m
@@ -30,32 +30,57 @@
     ClassB *clsB2 = [ClassB new];
     clsB2.name = @"clsB2";
     
-    TestClass *testClass1 = [TestClass objectWithData:@{
-                                                       @"name" : @"test",
-                                                       @"tureAge" : @"18" ,
-                                                       @"classA" : @{
-                                                               @"name" : @"clsA" ,
-                                                               @"classB" : @{
-                                                                       @"name" : @"cls2B"
-                                                                       },
-                                                               },
-                                                       @"classB" : @[clsB,clsB2]
-                                                       }];
-    NSLog(@"testClass1 : %@",testClass1);
+//    TestClass *testClass1 = [TestClass objectWithData:@{
+//                                                       @"name" : @"test",
+//                                                       @"tureAge" : @"18" ,
+//                                                       @"classA" : @{
+//                                                               @"name" : @"clsA" ,
+//                                                               @"classB" : @{
+//                                                                       @"name" : @"cls2B"
+//                                                                       },
+//                                                               },
+//                                                       @"classB" : @[clsB,clsB2]
+//                                                       }];
+//    NSLog(@"testClass1 : %@",testClass1);
+//    
+//    NSLog(@"testClass1 json : %@",testClass1.jsonString);
+//    
+//    TestClass *testClass2 = [TestClass objectWithJSONString:testClass1.jsonString];
+//    
+//    NSLog(@"testClass2 json : %@",testClass2.jsonString);
+//    
+//    NSArray *arr1 = @[testClass1,testClass2];
+//    
+//    NSString *arrJSON = [TestClass jsonStringFromObjectArray:arr1];
+//    NSLog(@"arrJSON : %@",arrJSON);
+//    
+//    NSArray *arr2 = [TestClass objectArrayFromJSONString:arrJSON];
+//    NSLog(@"arr2 : %@",[[arr2 description] stringByReplacingOccurrencesOfString:@"\\n" withString:@"\n"]);
     
-    NSLog(@"testClass1 json : %@",testClass1.jsonString);
     
-    TestClass *testClass2 = [TestClass objectWithJSONString:testClass1.jsonString];
+    NSTimeInterval begin = [[NSDate date] timeIntervalSince1970];
     
-    NSLog(@"testClass2 json : %@",testClass2.jsonString);
+    for (int i = 0; i < 10000; i++) {
+        TestClass *testClass1 = [TestClass objectWithData:@{
+                                                            @"name" : @"test",
+                                                            @"tureAge" : @"18" ,
+                                                            @"classA" : @{
+                                                                    @"name" : @"clsA" ,
+                                                                    @"classB" : @{
+                                                                            @"name" : @"cls2B"
+                                                                            },
+                                                                    },
+                                                            @"classB" : @[clsB,clsB2]
+                                                            }];
+        
+        TestClass *testClass2 = [TestClass objectWithJSONString:testClass1.jsonString];
+        NSString *arrJSON = [TestClass jsonStringFromObjectArray:@[testClass1,testClass2]];
+        NSArray *arr = [TestClass objectArrayFromJSONString:arrJSON];
+    }
     
-    NSArray *arr1 = @[testClass1,testClass2];
+    NSTimeInterval end = [[NSDate date] timeIntervalSince1970];
     
-    NSString *arrJSON = [TestClass jsonStringFromObjectArray:arr1];
-    NSLog(@"arrJSON : %@",arrJSON);
-    
-    NSArray *arr2 = [TestClass objectArrayFromJSONString:arrJSON];
-    NSLog(@"arr2 : %@",[[arr2 description] stringByReplacingOccurrencesOfString:@"\\n" withString:@"\n"]);
+    NSLog(@"cost time : %lf",(end - begin));
 }
 
 @end
