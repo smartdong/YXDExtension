4803a415cf5af51f303adf0d4c66b36b00d4c276
yangxudong <yangxudongsmart@gmail.com>
2016-03-22 16:27:05 +0800

    update

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDBaseClassExtension/YXDBaseViewController.m
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDBaseClassExtension/YXDBaseViewController.m
@@ -9,8 +9,31 @@
 
 @implementation YXDBaseViewController
 
--(void)dealloc {
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+//    if ([self.navigationController respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
+//        self.navigationController.interactivePopGestureRecognizer.delegate = nil;
+//    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+//    if (self.navigationController.viewControllers.firstObject != self) {
+//        self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"xxx"] style:UIBarButtonItemStylePlain target:self action:@selector(popViewController)];
+//    }
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
     NSLog(@"%@ : %@",self,NSStringFromSelector(_cmd));
 }
+
+#pragma mark - Actions
+
+//- (void)popViewController {
+//    [self.navigationController popViewControllerAnimated:YES];
+//}
 
 @end
