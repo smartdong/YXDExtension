30600b3693f4cd114b609fa53ffe5a1a52997337
YangXudong <yangxudongsmart@gmail.com>
2016-03-25 23:27:40 +0800

    为 TestClass 添加测试 property

--- YXDExtensionDemo/YXDExtensionDemo/Test/TestClass.h
+++ YXDExtensionDemo/YXDExtensionDemo/Test/TestClass.h
@@ -7,6 +7,7 @@
 //
 
 #import "YXDBaseObject.h"
+#import <CoreGraphics/CGBase.h>
 
 @class ClassA;
 @class ClassB;
@@ -26,5 +27,28 @@
 @property (nonatomic, strong) NSArray<NSString *> *arrayD;
 
 @property (nonatomic, strong) NSMutableDictionary *data;
+
+#pragma mark - Test Property
+
+//下面是支持的类型
+@property (nonatomic, strong) NSString              *propertyString;
+@property (nonatomic, strong) NSNumber              *propertyNumber;
+@property (nonatomic, strong) NSArray               *propertyArray;
+@property (nonatomic, strong) NSDictionary          *propertyDictionary;
+@property (nonatomic, assign) Class                 *propertyClass;
+@property (nonatomic, strong) dispatch_block_t       propertyBlock;
+@property (nonatomic, strong) NSObject              *propertyObject;
+@property (nonatomic, assign) SEL                   *propertySEL;
+@property (nonatomic, assign) NSInteger              propertyInteger;
+@property (nonatomic, assign) CGFloat                propertyFloat;
+@property (nonatomic, assign) BOOL                   propertyBool;
+
+//下面是测试捣乱的类型
+@property (nonatomic, strong) NSMutableString       *propertyMutableString;
+@property (nonatomic, strong) NSMutableArray        *propertyMutableArray;
+@property (nonatomic, strong) NSMutableDictionary   *propertyMutableDictionary;
+@property (nonatomic, assign) int                    propertyInt;
+@property (nonatomic, assign) double                 propertyDouble;
+@property (nonatomic, assign) Boolean                propertyBoolean;
 
 @end
