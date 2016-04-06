515d4e7857ac6da3435b643da053174e3aecbffd
yangxudong <yangxudongsmart@gmail.com>
2016-04-06 16:36:41 +0800

    更新测试方法

--- YXDExtensionDemo/YXDExtensionDemo/ViewController.m
+++ YXDExtensionDemo/YXDExtensionDemo/ViewController.m
@@ -29,61 +29,28 @@
 
     ClassB *clsB2 = [ClassB new];
     clsB2.name = @"clsB2";
-    
-    [YXDCommonFunction printTimeCost:^{
-        TestClass *testClass1 = [TestClass objectWithData:@{
-                                                            @"name" : @"test",
-                                                            @"tureAge" : @"18" ,
-                                                            @"classA" : @{
-                                                                    @"name" : @"clsA" ,
-                                                                    @"classB" : @{
-                                                                            @"name" : @"cls2B"
-                                                                            },
-                                                                    },
-                                                            @"classB" : @[clsB,clsB2],
-                                                            @"classC" : @[@{@"key1":@{@"v1":@"v2"}},@{@"key2":@"value2"}],
-                                                            @"classD" : @[@"1",@"2"],
-                                                            @"returnData" : @{@"hehe":@"haha",@"hengheng":@{@"a":@"b"}}
-                                                            }];
-        NSLog(@"testClass1 : %@",[testClass1 descriptionWithPropertyValues]);
-        
-        NSLog(@"testClass1 json : %@",testClass1.jsonString);
-        
-        TestClass *testClass2 = [TestClass objectWithJSONString:testClass1.jsonString];
-        
-        NSLog(@"testClass2 json : %@",testClass2.jsonString);
-    }];
 
-//    NSArray *arr1 = @[testClass1,testClass2];
-//    
-//    NSString *arrJSON = [TestClass jsonStringFromObjectArray:arr1];
-//    NSLog(@"arrJSON : %@",arrJSON);
-//    
-//    NSArray *arr2 = [TestClass objectArrayFromJSONString:arrJSON];
-//    NSLog(@"arr2 : %@",[[arr2 description] stringByReplacingOccurrencesOfString:@"\\n" withString:@"\n"]);
-    
-//    [YXDCommonFunction printTimeCost:^{
-//        for (int i = 0; i < 10000; i++) {
-//            TestClass *testClass1 = [TestClass objectWithData:@{
-//                                                                @"name" : @"test",
-//                                                                @"tureAge" : @"18" ,
-//                                                                @"classA" : @{
-//                                                                        @"name" : @"clsA" ,
-//                                                                        @"classB" : @{
-//                                                                                @"name" : @"cls2B"
-//                                                                                },
-//                                                                        },
-//                                                                @"classB" : @[clsB,clsB2],
-//                                                                @"classC" : @[@{@"key1":@{@"v1":@"v2"}},@{@"key2":@"value2"}],
-//                                                                @"classD" : @[@"1",@"2"],
-//                                                                @"returnData" : @{@"hehe":@"haha",@"hengheng":@{@"a":@"b"}}
-//                                                                }];
-//            
-//            TestClass *testClass2 = [TestClass objectWithJSONString:testClass1.jsonString];
-//            NSString *arrJSON = [TestClass jsonStringFromObjectArray:@[testClass1,testClass2]];
-//            NSArray *arr = [TestClass objectArrayFromJSONString:arrJSON];
-//        }
-//    }];
+    [YXDCommonFunction printTimeCost:^{
+        for (int i = 0; i < 10000; i++) {
+            TestClass *testClass1 = [TestClass objectWithData:@{
+                                                                @"name" : @"test",
+                                                                @"tureAge" : @"18" ,
+                                                                @"classA" : @{
+                                                                        @"name" : @"clsA" ,
+                                                                        @"classB" : @{
+                                                                                @"name" : @"cls2B"
+                                                                                },
+                                                                        },
+                                                                @"classB" : @[clsB,clsB2],
+                                                                @"classC" : @[@{@"key1":@{@"v1":@"v2"}},@{@"key2":@"value2"}],
+                                                                @"classD" : @[@"1",@"2"],
+                                                                @"returnData" : @{@"hehe":@"haha",@"hengheng":@{@"a":@"b"}}
+                                                                }];
+            TestClass *testClass2 = [TestClass objectWithJSONString:testClass1.jsonString];
+            NSString *arrJSON = [TestClass jsonStringFromObjectArray:@[testClass1,testClass2]];
+            NSArray *arr = [TestClass objectArrayFromJSONString:arrJSON];
+        }
+    }];
 }
 
 @end
