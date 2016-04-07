9879b0f43cbc6edd53e179ba7e90380a6f3fef9a
yangxudong <yangxudongsmart@gmail.com>
2016-04-07 17:56:52 +0800

    添加测试代码 测试 UIScrollView+YXDExtension 功能

--- YXDExtensionDemo/YXDExtensionDemo/Test/VC/RefreshTestTableViewController.m
+++ YXDExtensionDemo/YXDExtensionDemo/Test/VC/RefreshTestTableViewController.m
@@ -7,8 +7,11 @@
 
 #import "RefreshTestTableViewController.h"
 #import "UITableView+YXDExtension.h"
+#import "UIScrollView+YXDExtension.h"
 
 @interface RefreshTestTableViewController () <UITableViewDataSource,UITableViewDelegate>
+
+@property (nonatomic, assign) NSInteger count;
 
 @end
 
@@ -19,12 +22,33 @@
     // Do any additional setup after loading the view.
     
     [self.tableView registerDefaultCell];
+    
+    [self.tableView addLoadStatusViewWithPullLoadingBlock:^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            self.count = 10;
+            [self.tableView setStatusSuccess];
+        });
+    } footerLoadingBlock:^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            if (self.count < 50) {
+                self.count += 10;
+            } else {
+                [self.tableView noticeFooterNoMoreData];
+            }
+            [self.tableView setStatusSuccess];
+        });
+    }];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self.tableView triggerLoading];
 }
 
 #pragma mark - Table View
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 5;
+    return self.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
