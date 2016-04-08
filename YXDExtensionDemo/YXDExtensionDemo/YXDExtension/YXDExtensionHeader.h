932240a9c03c97aa3c4900393f3bb96d76166a6f
yangxudong <yangxudongsmart@gmail.com>
2016-04-08 15:03:19 +0800

    添加ddLogLevel定义

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDExtensionHeader.h
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDExtensionHeader.h
@@ -14,5 +14,10 @@
 
 #import "CocoaLumberjack.h"
 
+#ifdef DEBUG
+    static const DDLogLevel ddLogLevel = DDLogLevelVerbose;
+#else
+    static const DDLogLevel ddLogLevel = DDLogLevelError;
+#endif
 
 
