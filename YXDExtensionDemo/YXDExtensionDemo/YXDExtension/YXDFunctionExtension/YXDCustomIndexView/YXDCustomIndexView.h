c6eb8bf2f9a168c5c550c9ab867618fb47026331
yangxudong <yangxudongsmart@gmail.com>
2016-04-11 11:32:27 +0800

    添加YXDCustomIndexView

---
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDCustomIndexView/YXDCustomIndexView.h
@@ -0,0 +1,34 @@
+//
+//  YXDCustomIndexView.h
+//  YXDExtensionDemo
+//
+//  Copyright (c) 2015年 YangXudong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class YXDCustomIndexView;
+
+@protocol YXDCustomIndexViewDelegate <NSObject>
+
+/**
+ *  选择了某个索引
+ */
+- (void) customIndexView:(YXDCustomIndexView *)customView selectedSectionIndex:(int)index;
+
+@end
+
+
+@interface YXDCustomIndexView : UIView
+
+@property (nonatomic, weak) id<YXDCustomIndexViewDelegate> delegate;
+
+@property (nonatomic, strong) NSArray *sectionTitleArray;
+
++ (YXDCustomIndexView *) customIndexViewWithWidth:(float)width
+                                           center:(CGPoint)center
+                                sectionTitleArray:(NSArray *)sectionTitleArray
+                                         delegate:(id)delegate
+                                        superView:(UIView *)superView;
+
+@end
