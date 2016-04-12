e51dad1aa0bd1f0c2baa61d4408dc5d94e70de64
yangxudong <yangxudongsmart@gmail.com>
2016-04-12 16:27:10 +0800

    强迫症发作

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDExtensionDefine.h
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDExtensionDefine.h
@@ -9,8 +9,8 @@ static NSString *kYXDExtensionErrorDomain = @"com.yxd.demo";
 
 typedef NS_ENUM(NSInteger, YXDExtensionErrorCode) {
     
-    YXDExtensionErrorCodeUndefine     = -1,
     YXDExtensionErrorCodeSuccess      = 0,
+    YXDExtensionErrorCodeUndefine     = -1,
     
     //网络相关
     YXDExtensionErrorCodeServerError  = 500,
