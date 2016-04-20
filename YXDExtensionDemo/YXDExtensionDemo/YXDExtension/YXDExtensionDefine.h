597757badfc70c7c5783b41a19f07a70996e150d
yangxudong <yangxudongsmart@gmail.com>
2016-04-20 13:08:02 +0800

    YXDExtensionDefine update

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDExtensionDefine.h
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDExtensionDefine.h
@@ -5,17 +5,12 @@
 //  Copyright © 2016年 YangXudong. All rights reserved.
 //
 
-static NSString *kYXDExtensionErrorDomain = @"com.yxd.demo";
+#define kYXDExtensionErrorDomain [[NSBundle mainBundle] bundleIdentifier]
 
 typedef NS_ENUM(NSInteger, YXDExtensionErrorCode) {
-    
-    YXDExtensionErrorCodeSuccess      = 0,
-    YXDExtensionErrorCodeUndefine     = -1,
-    
-    //网络相关
-    YXDExtensionErrorCodeServerError  = 500,
-    YXDExtensionErrorCodeLostNetwork  = -100,
-    
-    //数据库相关
-    YXDExtensionErrorCodeInputError   = 100,
+    YXDExtensionErrorCodeUndefine     = -1      ,   //未定义
+    YXDExtensionErrorCodeSuccess      = 0       ,   //成功
+    YXDExtensionErrorCodeServerError  = 500     ,   //服务器内部错误
+    YXDExtensionErrorCodeLostNetwork  = -100    ,   //当前无网络
+    YXDExtensionErrorCodeInputError   = 100     ,   //输入错误
 };
