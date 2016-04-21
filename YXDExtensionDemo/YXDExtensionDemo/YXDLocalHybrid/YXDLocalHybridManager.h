9f96748057cef3a6edf82655402427b7b124cdce
yangxudong <yangxudongsmart@gmail.com>
2016-04-21 10:17:05 +0800

    add YXDLocalHybrid

---
+++ YXDExtensionDemo/YXDExtensionDemo/YXDLocalHybrid/YXDLocalHybridManager.h
@@ -0,0 +1,12 @@
+//
+//  YXDLocalHybridManager.h
+//  YXDExtensionDemo
+//
+//  Copyright © 2016年 YangXudong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YXDLocalHybridManager : NSObject
+
+@end
---
+++ YXDExtensionDemo/YXDExtensionDemo/YXDLocalHybrid/YXDLocalHybridManager.m
@@ -0,0 +1,12 @@
+//
+//  YXDLocalHybridManager.m
+//  YXDExtensionDemo
+//
+//  Copyright © 2016年 YangXudong. All rights reserved.
+//
+
+#import "YXDLocalHybridManager.h"
+
+@implementation YXDLocalHybridManager
+
+@end
