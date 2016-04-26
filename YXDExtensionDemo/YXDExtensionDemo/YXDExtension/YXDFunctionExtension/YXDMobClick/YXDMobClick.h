b1a6a07ce1b15c7e2ed5b4f0d1133601e383a3b3
yangxudong <yangxudongsmart@gmail.com>
2016-04-26 15:15:23 +0800

    add YXDMobClick

---
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDMobClick/YXDMobClick.h
@@ -0,0 +1,12 @@
+//
+//  YXDMobClick.h
+//  YXDExtensionDemo
+//
+//  Copyright © 2016年 YangXudong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YXDMobClick : NSObject
+
+@end
---
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDMobClick/YXDMobClick.m
@@ -0,0 +1,12 @@
+//
+//  YXDMobClick.m
+//  YXDExtensionDemo
+//
+//  Copyright © 2016年 YangXudong. All rights reserved.
+//
+
+#import "YXDMobClick.h"
+
+@implementation YXDMobClick
+
+@end
