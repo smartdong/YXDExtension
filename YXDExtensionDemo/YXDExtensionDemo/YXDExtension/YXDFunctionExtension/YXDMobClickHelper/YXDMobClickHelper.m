18d7664732011b1f75000ed6a5fcde1fbc5de563
yangxudong <yangxudongsmart@gmail.com>
2016-04-26 15:50:14 +0800

    YXDMobClickHelper update

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDMobClickHelper/YXDMobClickHelper.m
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDMobClickHelper/YXDMobClickHelper.m
@@ -6,7 +6,47 @@
 //
 
 #import "YXDMobClickHelper.h"
+#import "Aspects.h"
+#import "YXDCommonFunction.h"
+#import "YXDNetworkManager.h"
+#import "YXDNetworkResult.h"
+
+static NSString *YXDMobClickHelperAutoConfigKey = @"YXDMobClickHelperAutoConfigKey";
 
 @implementation YXDMobClickHelper
+
++ (void)autoConfigWithUrl:(NSString *)url params:(NSDictionary *)params eventBlock:(void (^)(NSString *))eventBlock {
+    if (!url.length || !eventBlock) {
+        return;
+    }
+    
+    //存在本地的配置
+    NSArray<NSDictionary *> *config = [YXDCommonFunction userDefaultsValueForKey:YXDMobClickHelperAutoConfigKey];
+    //hook 统计事件
+    [config enumerateObjectsUsingBlock:^(NSDictionary * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([obj isKindOfClass:[NSDictionary class]]) {
+            NSString *clazz = [obj objectForKey:@"class"];
+            NSString *method = [obj objectForKey:@"method"];
+            NSString *event = [obj objectForKey:@"event"];
+            if (clazz.length && method.length && event.length) {
+                [NSClassFromString(clazz) aspect_hookSelector:NSSelectorFromString(method)
+                                                  withOptions:AspectPositionAfter
+                                                   usingBlock:^{eventBlock(event);}
+                                                        error:nil];
+            }
+        }
+    }];
+    
+    //更新最新的 config
+    [[YXDNetworkManager newManager] sendRequestWithParams:params
+                                         interfaceAddress:url
+                                                  success:^(YXDNetworkResult *result) {
+                                                      if (result.list.count) {
+                                                          [YXDCommonFunction setUserDefaultsValue:result.list forKey:YXDMobClickHelperAutoConfigKey];
+                                                      }
+                                                  }
+                                            loadingStatus:nil
+                                                   method:POST];
+}
 
 @end
