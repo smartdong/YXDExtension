f6f6852e4193f27e8747447a6ba43ff9d8218417
yangxudong <yangxudongsmart@gmail.com>
2016-06-16 11:57:13 +0800

    add test class

---
+++ YXDExtensionDemo/YXDExtensionDemo/Test/Class/GirlFriend.h
@@ -0,0 +1,19 @@
+//
+//  GirlFriend.h
+//  YXDExtensionDemo
+//
+//  Created by zjdd on 16/6/16.
+//  Copyright © 2016年 YangXudong. All rights reserved.
+//
+
+#import "YXDBaseObject.h"
+
+@interface GirlFriend : YXDBaseObject
+
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSNumber *age;
+@property (nonatomic, strong) NSNumber *height;
+@property (nonatomic, strong) NSNumber *weight;
+@property (nonatomic, strong) NSString *size;
+
+@end
---
+++ YXDExtensionDemo/YXDExtensionDemo/Test/Class/GirlFriend.m
@@ -0,0 +1,13 @@
+//
+//  GirlFriend.m
+//  YXDExtensionDemo
+//
+//  Created by zjdd on 16/6/16.
+//  Copyright © 2016年 YangXudong. All rights reserved.
+//
+
+#import "GirlFriend.h"
+
+@implementation GirlFriend
+
+@end
