153ac6f985bfab35b9cd3276c4d89bc968dc1d87
yangxudong <yangxudongsmart@gmail.com>
2016-06-16 11:46:42 +0800

    YXDBaseObject add property primaryID

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDBaseClassExtension/YXDBaseObject.h
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDBaseClassExtension/YXDBaseObject.h
@@ -9,6 +9,9 @@
 
 @interface YXDBaseObject : NSObject
 
+//数据库存储主键
+@property (nonatomic, strong) NSNumber *primaryID;
+
 //属性名称和接口返回值名称的对应关系  @{@"属性名称" : @"返回值名称"}
 //如果对象属性包含数组  则对应关系为 @{@"属性名称" : @{@"返回值名称" : [xxx class]}}
 - (NSDictionary *)propertyMap;
