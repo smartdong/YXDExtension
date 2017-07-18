abe53872890469461cfa61e0c7592adecfa81fac
yangxudong <yangxudongsmart@gmail.com>
2017-07-18 17:50:46 +0800

    NSNull+YXDExtension update.

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFoundationExtension/NSNull+YXDExtension.m
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFoundationExtension/NSNull+YXDExtension.m
@@ -25,4 +25,28 @@
     return 0;
 }
 
+- (NSInteger)integerValue {
+    return 0;
+}
+
+- (NSString *)description {
+    return nil;
+}
+
+- (int)intValue {
+    return 0;
+}
+
+- (float)floatValue {
+    return 0;
+}
+
+- (double)doubleValue {
+    return 0;
+}
+
+- (BOOL)boolValue {
+    return false;
+}
+
 @end
