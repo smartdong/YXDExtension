3c0359f7cfa26cf3a5a492c8a7e2a321a37b1bed
yangxudong <yangxudongsmart@gmail.com>
2017-09-10 15:51:43 +0800

    YXDDevicePushTokenManager added.

---
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDDevicePushTokenManager/YXDDevicePushTokenManager.h
@@ -0,0 +1,18 @@
+//
+//  YXDDevicePushTokenManager.h
+//  YXDExtensionDemo
+//
+//  Copyright © 2017年 YangXudong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YXDDevicePushTokenManager : NSObject
+
+//保存最新的 token
++ (void)saveDeviceToken:(NSData *)deviceToken;
+
+//判断是否需要更新 token 如果需要则会回调 updateBlock 上传成功后需要回调 successCompletion
++ (void)autoUpdateBlock:(void(^)(NSString *deviceToken, dispatch_block_t successCompletion))updateBlock;
+
+@end
---
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDFunctionExtension/YXDDevicePushTokenManager/YXDDevicePushTokenManager.m
@@ -0,0 +1,57 @@
+//
+//  YXDDevicePushTokenManager.m
+//  YXDExtensionDemo
+//
+//  Copyright © 2017年 YangXudong. All rights reserved.
+//
+
+#import "YXDDevicePushTokenManager.h"
+#import "YXDCommonFunction.h"
+
+@interface NSData (YXDDeviceToken)
+
+- (NSString *)deviceTokenString;
+
+@end
+
+@implementation NSData (YXDDeviceToken)
+
+- (NSString *)deviceTokenString {
+    return [[self.description stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]] stringByReplacingOccurrencesOfString:@" " withString:@""];
+}
+
+@end
+
+static NSString *const kYXDCurrentDeviceTokenStorageKey     = @"kYXDCurrentDeviceTokenStorageKey";
+static NSString *const kYXDUploadedDeviceTokenStorageKey    = @"kYXDUploadedDeviceTokenStorageKey";
+
+@implementation YXDDevicePushTokenManager
+
++ (void)saveDeviceToken:(NSData *)deviceToken {
+    [YXDCommonFunction setUserDefaultsValue:deviceToken.deviceTokenString forKey:kYXDCurrentDeviceTokenStorageKey];
+}
+
++ (NSString *)currentDeviceToken {
+    return [YXDCommonFunction userDefaultsValueForKey:kYXDCurrentDeviceTokenStorageKey];
+}
+
++ (void)saveUploadedDeviceToken:(NSString *)deviceTokenString {
+    [YXDCommonFunction setUserDefaultsValue:deviceTokenString forKey:kYXDUploadedDeviceTokenStorageKey];
+}
+
++ (NSString *)uploadedDeviceToken {
+    return [YXDCommonFunction userDefaultsValueForKey:kYXDUploadedDeviceTokenStorageKey];
+}
+
++ (void)autoUpdateBlock:(void(^)(NSString *deviceToken, dispatch_block_t successCompletion))updateBlock {
+    NSString *currentDeviceToken = [YXDDevicePushTokenManager currentDeviceToken];
+    NSString *uploadedDeviceToken = [YXDDevicePushTokenManager uploadedDeviceToken];
+    
+    BOOL uploaded = [currentDeviceToken isEqualToString:uploadedDeviceToken];
+    
+    if (!uploaded && updateBlock) {
+        updateBlock(currentDeviceToken, ^{[YXDDevicePushTokenManager saveUploadedDeviceToken:currentDeviceToken];});
+    }
+}
+
+@end
