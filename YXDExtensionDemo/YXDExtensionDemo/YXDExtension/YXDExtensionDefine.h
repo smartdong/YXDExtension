99d8d0bb14009d65bdbf5de40ef071982a888e51
YangXudong <yangxudongsmart@gmail.com>
2018-06-03 14:03:42 +0800

    Update. Update YXDExtensionErrorCode define.

--- YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDExtensionDefine.h
+++ YXDExtensionDemo/YXDExtensionDemo/YXDExtension/YXDExtensionDefine.h
@@ -8,12 +8,12 @@
 #define kYXDExtensionErrorDomain [[NSBundle mainBundle] bundleIdentifier]
 
 typedef NS_ENUM(NSInteger, YXDExtensionErrorCode) {
-    YXDExtensionErrorCodeUndefine     = -1      ,   //未定义
-    YXDExtensionErrorCodeSuccess      = 0       ,   //成功
-    YXDExtensionErrorCodeServerError  = 500     ,   //服务器内部错误
-    YXDExtensionErrorCodeLostNetwork  = -100    ,   //当前无网络
-    YXDExtensionErrorCodeInputError   = 100     ,   //输入错误
-    YXDExtensionErrorCodeCancelled    = -999    ,   //用户取消
+    YXDExtensionErrorCodeSuccess      = 0,      //成功
+    YXDExtensionErrorCodeUndefine     = 1,      //未定义
+    YXDExtensionErrorCodeLostNetwork  = 100,    //当前无网络
+    YXDExtensionErrorCodeInputError   = 300,    //输入错误
+    YXDExtensionErrorCodeCancelled    = 400,    //用户取消
+    YXDExtensionErrorCodeServerError  = 500,    //服务器内部错误
 };
 
 static NSString *const kYXDExtensionErrorUnknown = @"未知错误";
